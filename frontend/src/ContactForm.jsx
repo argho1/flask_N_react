3372f52b971c7713f2397e0040b102c93124b6a7
Argho Sinha <argho1@live.com>
2024-08-26 22:11:33 +0530

    added styling to create contact

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,9 +1,11 @@
 import {useState} from "react"
 
-const ContactForm = ({}) => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
+const ContactForm = ({ existingContacts = {}, updateCallback}) => {
+    const [firstName, setFirstName] = useState(existingContacts.firstName || "")
+    const [lastName, setLastName] = useState(existingContacts.lastName || "")
+    const [email, setEmail] = useState(existingContacts.email || "")
+
+    const updating = Object.entries(existingContacts).length !== 0
 
     const onSubmit = async (e) => {
         e.preventDefault()
@@ -14,7 +16,7 @@ const ContactForm = ({}) => {
             email
         }
 
-        const url = "http://127.0.0.1:5000/create_contact"
+        const url = "http://127.0.0.1:5000/" + (updateing ? 'update_contact/${existingContact.id}' : 'create_contact')
         const options = {
             method: "POST",
             headers: {
@@ -27,7 +29,7 @@ const ContactForm = ({}) => {
             const data = await response.json()
             alert(data.message)
         } else {
-            
+            updateCallback()
         }
     }
 
